e955c68bcaad96e2b0d8db6ff89fa93c6de388e9
Martin Dengler <martin@martindengler.com>
2012-03-28 02:25:31 +0800

    upgrade jtsclient.jar to 9.6.7

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [clj-time "0.3.0"]
                  ;; download this from IB and add it to your local m2
                  ;; repo, as per leiningen's instructions
-                 [jtsclient "9.6.5"]]
+                 [jtsclient "9.6.7"]]
   :dev-dependencies [[midje "1.3-alpha4"]
                      [lein-exec "0.1"]
                      [lein-marginalia "0.6.1"]])
